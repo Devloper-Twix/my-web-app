38d97fc8388e29bbc0877730ecac846adde37c39
Devloper-Twix <155011651+Devloper-Twix@users.noreply.github.com>
2024-09-19 17:34:40 +0530

    v0.2.1

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,52 +1,12 @@
-import * as React from "react";
+import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-export function LoginForm() {
+const page = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>
-            Access your account using your credentials.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="Enter your email" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder="Enter your password"
-                />
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Login</Button>
-        </CardFooter>
-      </Card>
+    <div>
+      <SpeedInsights></SpeedInsights>
     </div>
   );
-}
+};
 
-export default LoginForm;
+export default page;
